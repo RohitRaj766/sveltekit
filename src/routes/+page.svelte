89a192a8642a5372a38fc9e547b25df6224379e4
RohitRaj766 <rohitraj2k04@gmail.com>
2024-07-05 14:23:56 +0530

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="hover:underline p-10 text-red-500 font-bold cursor-pointer">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,7 +1,8 @@
 <script>
     export let data
+    import '../../../app.css'
 </script>
 
-<p>
+<p class="font-light text-center hover:underline text-red-700">
     {data.content}
 </p>
